1afc8a1c09ebeb67f149e96bd406e7c99d3f4af1
ashleydillon <ashley.dillon@sabre.com>
2017-03-21 14:08:20 -0500

    form pop update

--- unit_1/formpop.js
+++ unit_1/formpop.js
@@ -1,25 +1,25 @@
-// JavaScript Document
-<script>
-$(document).ready(function() {
-    // config
-        popup = $('#popup');
-        clickme = $('.contact');
+// Get the modal
+var modal = document.getElementById('myModal');
 
-    // pop-up
-        vh = $(window).height();
-        vw = $(window).width();
-        bw = popup.width();
-        bh = popup.height();
-        clickme.click(function(e) {
-            e.preventDefault();
-            popup.fadeOut();
-            popup.css('left', vw/2 - bw/2);
-            popup.css('top', vh/2 - bh/2);
-            popup.fadeIn();
-        });
-   //close button
-      $('.clicktoclose').click(function() {
-            $('#popup').fadeOut();
-        });
-});
-</script>
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
